b9ef4fd51b4e04a0985d2a115b73f32af7d11fdd
roLLlyTHuK <ircvipua@gmail.com>
2023-12-26 22:38:13 +0200

    up

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { ChatContextProvider } from './context/ChatContext';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AuthContextProvider>
     <ChatContextProvider>
       <React.StrictMode>
-        <App />
+        <BrowserRouter basename="/pet-chat">
+          <App />
+        </BrowserRouter>
       </React.StrictMode>
     </ChatContextProvider>
   </AuthContextProvider>
