f97ac88837c088aa99e892e53be7cda3e2b9b8b2
Andriy Dovhaniuk <71319633+roLLlyTHuK@users.noreply.github.com>
2023-12-27 14:27:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './style.scss';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
